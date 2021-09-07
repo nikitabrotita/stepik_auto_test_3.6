63564416bfd658036c80e40b5049fe1a54205198
nikitabrotita <53574997+nikitabrotita@users.noreply.github.com>
2021-09-07 16:45:53 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -4,12 +4,10 @@ from selenium import webdriver
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-class TestMainPage1():
-
-    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         print("start test")
         browser.get(link)
-	time.sleep(30)
+	sleep(30)
         add_to_basket = browser.find_element_by_css_selector(".btn-primary")
         print("finish test")
 	assert add_to_basket, 'кнопка добавления товара не существует'
