b2b6e69a361878f98f86f344f184512908d60fa5
nikitabrotita <53574997+nikitabrotita@users.noreply.github.com>
2021-09-07 16:50:26 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -5,9 +5,9 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_guest_should_see_basket_link_on_the_main_page(self, browser):
-        print("start test")
-        browser.get(link)
-	sleep(30)
-        add_to_basket = browser.find_element_by_css_selector(".btn-primary")
-        print("finish test")
-	assert add_to_basket, 'кнопка добавления товара не существует'
+    print("start test")
+    browser.get(link)
+    sleep(30)
+    add_to_basket = browser.find_element_by_css_selector(".btn-primary")
+    print("finish test")
+    assert add_to_basket, 'кнопка добавления товара не существует'
